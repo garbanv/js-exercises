089b69fc36dfddfbe67fe267b8092c31699c41cb
Garbanv <garban.valdeon@gmail.com>
2020-04-04 10:35:07 +0200

    exercises completed

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,10 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+// returns a random number from 0 to 9
 // - How many parameters does the function take?
+// in this case it doesnt have any parameters
 // - What is the function name?
+// getNumber
 // - Where is the function called? with what parameters?
+// its called inside result
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,14 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
+// It joins the words code and your (codeyour)
 // - How many parameters does the function take?
+// 2
 // - What is the function name?
+// s
 // - Where is the function called? with what parameters?
+// its called in the var result with w1 and w2 as parameters
 // - What does the function return?
+// return a string (codeyour)
 // - How can this function be improved?
+// Maybe as string is simple to join then with a simple + sign.
